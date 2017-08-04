6590a94e1f70fcaa9f75738137dcd7090a4f3df1
Lindsey Carboneau <lmcarboneau@gmail.com>
2017-08-04 17:20:17 -0400

    Changing the data input for missing graph

---
+++ src/4385148timeseries.js
@@ -0,0 +1,49 @@
+
+function timeseries() {
+    $.getJSON('./data/test4385148.csv', function(data) {
+            
+        Highcharts.chart('container', {
+            chart: {
+                zoomType: 'x'
+            },
+            title: {
+                text: 'AstroDev Webviewer'
+            },
+            subtitle: {
+                text: document.ontouchstart === undefined ? 
+                    'Click and drag in the plot area to zoom in' : 
+                    'Pinch in the plot area to zoom in'
+                    // changes for which device is used to view
+            },
+            legend: {
+                enabled: false
+            },
+            xAxis: {
+                type: 'linear',
+                title: {
+                    text: 'Cadence'
+                },
+            },
+            yAxis: {
+                title: {
+                    text: 'Flux'
+                }
+            },
+            data: {
+                csv: document.getElementById('csv').innerHTML
+            },
+            plotOptions: {
+                series: {
+                    marker: {
+                        enabled: false
+                    }
+                }
+            },
+            series: [{
+                name: 'Sample Test Data - Kepler ID 4385148',
+                data: data
+            }]
+        });
+
+    });
+}
