f182996b318b75e49c5f28c3224047e697725b08
Hunter Bedwell <bedwellhb@gmail.com>
2021-07-08 12:13:00 -0400

    The form now routes to the original FormSpree URL if all the fields are correct, otherwise the button is disabled.

--- bootstrap-4.1.3-dist/js/form-validation.js
+++ bootstrap-4.1.3-dist/js/form-validation.js
@@ -2,27 +2,25 @@ const name = document.getElementById("name");
 const email = document.getElementById("email");
 const subject = document.getElementById("subject");
 const message = document.getElementById("message");
-
 const submitButton = document.getElementById("submitButton");
-
-var nameIsValid = false;
-var emailIsValid = false;
-var subjectIsValid = false;
-var messageIsValid = false;
+const form = document.getElementById("contactForm");
 
 name.addEventListener("blur", valName);
 email.addEventListener("blur", valEmail);
 subject.addEventListener("blur", valSubject);
 message.addEventListener("blur", valMessage);
 
+var nameIsValid = false;
+var emailIsValid = false;
+var subjectIsValid = false;
+var messageIsValid = false;
+
 function valName() {
   if (name.value) {
     name.classList.remove("is-invalid");
-    submitButton.disabled = false;
     nameIsValid = true;
   } else {
     name.classList.add("is-invalid");
-    submitButton.disabled = true;
     nameIsValid = false;
   }
 }
@@ -31,11 +29,9 @@ function valEmail() {
   const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
   if (!re.test(email.value)) {
     email.classList.add("is-invalid");
-    submitButton.disabled = true;
     emailIsValid = false;
   } else {
     email.classList.remove("is-invalid");
-    submitButton.disabled = false;
     emailIsValid = true;
   }
 }
@@ -43,11 +39,9 @@ function valEmail() {
 function valSubject() {
   if (subject.value) {
     subject.classList.remove("is-invalid");
-    submitButton.disabled = false;
     subjectIsValid = true;
   } else {
     subject.classList.add("is-invalid");
-    submitButton.disabled = true;
     subjectIsValid = false;
   }
 }
@@ -55,21 +49,18 @@ function valSubject() {
 function valMessage() {
   if (message.value) {
     message.classList.remove("is-invalid");
-    submitButton.disabled = false;
     messageIsValid = true;
   } else {
     message.classList.add("is-invalid");
-    submitButton.disabled = true;
     messageIsValid = false;
   }
 }
 
-function formValidation() {
+function validateForm() {
   if (nameIsValid && emailIsValid && subjectIsValid && messageIsValid) {
-    alert("Validations successful!");
-    return true;
+    form.setAttribute("action", "https://formspree.io/f/{id}");
   } else {
-    returnToPreviousPage();
-    return false;
+    event.preventDefault();
   }
+  return true;
 }
