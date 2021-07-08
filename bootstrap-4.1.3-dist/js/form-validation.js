601f160ae55190fb34fc4f6c7f9f0cc8f92f80e6
Hunter Bedwell <bedwellhb@gmail.com>
2021-07-08 11:37:12 -0400

    Disabled the send message button on the contact form when errors are present.

--- bootstrap-4.1.3-dist/js/form-validation.js
+++ bootstrap-4.1.3-dist/js/form-validation.js
@@ -1,43 +1,75 @@
-const name = document.getElementById('name');
-const email = document.getElementById('email');
-const subject = document.getElementById('subject')
-const message = document.getElementById('message')
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
 
-name.addEventListener('blur', valName);
-email.addEventListener('blur', valEmail);
-subject.addEventListener('blur', valSubject);
-message.addEventListener('blur', valMessage);
+const submitButton = document.getElementById("submitButton");
+
+var nameIsValid = false;
+var emailIsValid = false;
+var subjectIsValid = false;
+var messageIsValid = false;
+
+name.addEventListener("blur", valName);
+email.addEventListener("blur", valEmail);
+subject.addEventListener("blur", valSubject);
+message.addEventListener("blur", valMessage);
 
 function valName() {
-  const re = /^[a-zA-Z ]+$/;
-  if(!re.test(name.value)) {
-    name.classList.add('is-invalid');
+  if (name.value) {
+    name.classList.remove("is-invalid");
+    submitButton.disabled = false;
+    nameIsValid = true;
   } else {
-    name.classList.remove('is-invalid');
+    name.classList.add("is-invalid");
+    submitButton.disabled = true;
+    nameIsValid = false;
   }
 }
 
 function valEmail() {
   const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-  if(!re.test(email.value)) {
-    email.classList.add('is-invalid');
+  if (!re.test(email.value)) {
+    email.classList.add("is-invalid");
+    submitButton.disabled = true;
+    emailIsValid = false;
   } else {
-    email.classList.remove('is-invalid');
+    email.classList.remove("is-invalid");
+    submitButton.disabled = false;
+    emailIsValid = true;
   }
 }
 
 function valSubject() {
-  if(subject.value) {
-    subject.classList.remove('is-invalid');
+  if (subject.value) {
+    subject.classList.remove("is-invalid");
+    submitButton.disabled = false;
+    subjectIsValid = true;
   } else {
-    subject.classList.add('is-invalid');
+    subject.classList.add("is-invalid");
+    submitButton.disabled = true;
+    subjectIsValid = false;
   }
 }
 
 function valMessage() {
-  if(message.value) {
-    message.classList.remove('is-invalid');
+  if (message.value) {
+    message.classList.remove("is-invalid");
+    submitButton.disabled = false;
+    messageIsValid = true;
   } else {
-    message.classList.add('is-invalid');
+    message.classList.add("is-invalid");
+    submitButton.disabled = true;
+    messageIsValid = false;
+  }
+}
+
+function formValidation() {
+  if (nameIsValid && emailIsValid && subjectIsValid && messageIsValid) {
+    alert("Validations successful!");
+    return true;
+  } else {
+    returnToPreviousPage();
+    return false;
   }
 }
